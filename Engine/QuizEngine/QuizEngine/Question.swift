1df409399441ea204688a959f1cf4ddce3e4771f
Shohin <tagayev.shohin@gmail.com>
2021-01-03 10:37:05 +0500

    Removed Hashable from Question

--- Engine/QuizEngine/QuizEngine/Question.swift
+++ Engine/QuizEngine/QuizEngine/Question.swift
@@ -10,15 +10,4 @@ import Foundation
 public enum Question<T: Hashable>: Hashable {
     case singleAnswer(T)
     case multipleAnswer(T)
-    
-    public func hash(into hasher: inout Hasher) {
-        switch self {
-        case .singleAnswer(let value):
-            let val = "singleAnswer_\(value)"
-            val.hash(into: &hasher)
-        case .multipleAnswer(let value):
-            let val = "multipleAnswer_\(value)"
-            val.hash(into: &hasher)
-        }
-    }
 }
