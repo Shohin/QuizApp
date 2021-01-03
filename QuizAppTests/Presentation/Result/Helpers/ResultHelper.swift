3333a7ab370520987d4bcc31315a6c772ec9c128
Shohin <tagayev.shohin@gmail.com>
2021-01-03 10:43:32 +0500

    Improved Hashable

--- QuizAppTests/Presentation/Result/Helpers/ResultHelper.swift
+++ QuizAppTests/Presentation/Result/Helpers/ResultHelper.swift
@@ -7,20 +7,21 @@
 
 @testable import QuizEngine
 
-extension Result: Hashable {
+extension Result {
     static func make(answers: [Question: Answer] = [:], score: Int = 0) -> Result<Question, Answer> {
         Result(answers: answers, score: score)
     }
-    
-    public var hashValue: Int {
-        1
-    }
-    
-    public func hash(into hasher: inout Hasher) {
-        
-    }
-    
+}
+
+extension Result: Equatable where Answer: Equatable {
     public static func == (lhs: Result<Question, Answer>, rhs: Result<Question, Answer>) -> Bool {
-        lhs.score == rhs.score
+        lhs.score == rhs.score && lhs.answers == rhs.answers
+    }
+}
+
+extension Result: Hashable where Answer: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(answers)
+        hasher.combine(score)
     }
 }
