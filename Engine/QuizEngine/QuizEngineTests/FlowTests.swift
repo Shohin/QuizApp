1e8fba01905014a0de6f504cb135e850ba395726
Shohin <tagayev.shohin@gmail.com>
2021-01-06 09:12:06 +0500

    Implemented QuizDelegate to FlowTests DelegateSpy

--- Engine/QuizEngine/QuizEngineTests/FlowTests.swift
+++ Engine/QuizEngine/QuizEngineTests/FlowTests.swift
@@ -132,18 +132,26 @@ class FlowTests: XCTestCase {
         Flow(questions: questions, router: delegate, scoring: scoring)
     }
     
-    private class DelegateSpy: Router {
+    private class DelegateSpy: Router, QuizDelegate {
         var routedQuestions: [String] = []
         var answerCallback: (String) -> Void = {_ in}
         var routedResult: Result<String, String>? = nil
         
-        func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
+        func handle(question: String, answerCallback: @escaping (String) -> Void) {
             routedQuestions.append(question)
             self.answerCallback = answerCallback
         }
         
-        func routeTo(result: Result<String, String>) {
+        func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
+            handle(question: question, answerCallback: answerCallback)
+        }
+        
+        func handle(result: Result<String, String>) {
             routedResult = result
+        }
+        
+        func routeTo(result: Result<String, String>) {
+            handle(result: result)
         }
     }
 }
