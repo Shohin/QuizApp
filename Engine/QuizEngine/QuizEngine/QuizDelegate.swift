bca91102d174232b82f0f35bd39e885afd6ea09f
Shohin <tagayev.shohin@gmail.com>
2021-01-07 08:23:04 +0500

    Moved QuizDelegate to its own file

---
+++ Engine/QuizEngine/QuizEngine/QuizDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  QuizDelegate.swift
+//  QuizEngine
+//
+//  Created by Shohin Tagaev on 07/01/21.
+//
+
+import Foundation
+
+protocol QuizDelegate {
+    associatedtype Question: Hashable
+    associatedtype Answer
+    
+    func handle(question: Question, answerCallback: @escaping (Answer) -> Void)
+    func handle(result: Result<Question, Answer>)
+}
