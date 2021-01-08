f865e2e7fd7581cd7f35456a835fcf284d3035ed
Shohin <tagayev.shohin@gmail.com>
2021-01-08 08:36:26 +0500

    Moved Quiz to its own file to production

---
+++ Engine/QuizEngine/QuizEngine/Quiz.swift
@@ -0,0 +1,23 @@
+//
+//  Quiz.swift
+//  QuizEngine
+//
+//  Created by Shohin Tagaev on 08/01/21.
+//
+
+import Foundation
+
+public final class Quiz {
+    private let flow: Any
+    init(flow: Any) {
+        self.flow = flow
+    }
+    
+    public static func start<Delegate: QuizDelegate> (questions: [Delegate.Question], delegate: Delegate, correctAnswers: [Delegate.Question: Delegate.Answer]) -> Quiz where Delegate.Answer: Equatable {
+        let flow = Flow(questions: questions, delegate: delegate) {
+            scoringGame(answers: $0, correctAnswers: correctAnswers)
+        }
+        flow.start()
+        return Quiz(flow: flow)
+    }
+}
--- Engine/QuizEngine/QuizEngineTests/QuizTests.swift
+++ Engine/QuizEngine/QuizEngineTests/QuizTests.swift
@@ -7,22 +7,7 @@
 
 import Foundation
 import XCTest
-@testable import QuizEngine
-
-final class Quiz {
-    private let flow: Any
-    init(flow: Any) {
-        self.flow = flow
-    }
-    
-    static func start<Delegate: QuizDelegate> (questions: [Delegate.Question], delegate: Delegate, correctAnswers: [Delegate.Question: Delegate.Answer]) -> Quiz where Delegate.Answer: Equatable {
-        let flow = Flow(questions: questions, delegate: delegate) {
-            scoringGame(answers: $0, correctAnswers: correctAnswers)
-        }
-        flow.start()
-        return Quiz(flow: flow)
-    }
-}
+import QuizEngine
 
 final class QuizTests: XCTestCase {
     private let delegate = DelegateSpy()
