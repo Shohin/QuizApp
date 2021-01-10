32789c29ee7a2d39ae75e62149fe705a3ab29f07
Shohin <tagayev.shohin@gmail.com>
2021-01-10 08:30:24 +0500

    deprecated handle(result:) method to favor didCompleteQuize(withAnswers:)

--- Engine/QuizEngine/QuizEngine/QuizDelegate.swift
+++ Engine/QuizEngine/QuizEngine/QuizDelegate.swift
@@ -13,5 +13,13 @@ public protocol QuizDelegate {
     
     func answer(for question: Question, completion: @escaping (Answer) -> Void)
     
+    func didCompleteQuize(withAnswers: [(question: Question, answer: Answer)])
+    
+    @available(*, deprecated)
     func handle(result: Result<Question, Answer>)
+}
+
+#warning("Remove this at some point")
+public extension QuizDelegate {
+    func didCompleteQuize(withAnswers: [(question: Question, answer: Answer)]) { }
 }
